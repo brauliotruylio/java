5d78cf7db986d1ede9fac9d9d9ee131f25fff7c4
Braulio Truylio <braulio.truylio@gmail.com>
2023-09-01 11:08:41 -0300

    update

--- ContaCorrente/src/main/java/org/conta/ContaCorrente.java
+++ ContaCorrente/src/main/java/org/conta/ContaCorrente.java
@@ -1,5 +1,63 @@
 package org.conta;
 
 public class ContaCorrente {
+    //Variáveis
+    int numeroConta;
+    String nome;
+    float saldo;
 
+    //Construtor
+    public ContaCorrente(int numeroConta, String nome, float saldo) {
+        this.numeroConta = numeroConta;
+        this.nome = nome;
+        this.saldo = 0.0f;
+    }
+
+    //Getters and Setters
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    //Métodos
+    public void alterarNome(String novoNome) {
+        nome = novoNome;
+    }
+
+    public void deposito(float valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
+    }
+
+    public void saque(float valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque de " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Saque não permitido. Saldo insuficiente ou valor inválido.");
+        }
+    }
 }
--- ContaCorrente/src/main/java/org/conta/Main.java
+++ ContaCorrente/src/main/java/org/conta/Main.java
@@ -1,10 +1,20 @@
 package org.conta;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
 
+        ContaCorrente conta1 = new ContaCorrente(12345, "Braulio", 0.0f);
 
+        System.out.println("Saldo inicial: " + conta1.getSaldo());
+
+        conta1.deposito(1000.0f);
+        System.out.println("Saldo após depósito: " + conta1.getSaldo());
+
+        conta1.saque(500.0f);
+        System.out.println("Saldo após saque: " + conta1.getSaldo());
+
+        conta1.alterarNome("João Silva");
+        System.out.println("Novo nome do correntista: " + conta1.nome);
     }
 }
